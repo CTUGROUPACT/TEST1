5362d6bb7e8acf1b24730e3474c510eb6093dee8
KP257 <Sebastiandebeer5@gmail.com>
2017-05-16 19:03:39 +0200

    Test Files

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String strOne = "world";
+            String strTwo = "Hello";
+            Console.WriteLine(SwapWords(strOne, strTwo));
+            
+                
+            Console.WriteLine(strOne +" " + strTwo );
+
+            Console.ReadKey(); 
+        }
+        static String SwapWords( string String1, string String2)
+        {
+            String strThree = String1;
+            String1 = String2;
+            String2 = strThree;
+            return (String1 + " " + String2);
+
+                
+        }
+        
+    }
+}
